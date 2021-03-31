c64429beb671b539223a3c4e2621a9cdc07bb9fc
Franke Telgenhof <franke.telgenhof@trivento.nl>
2021-03-31 11:41:19 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/frtelg/cadence/CadenceApplication.java
@@ -0,0 +1,13 @@
+package com.frtelg.cadence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadenceApplication.class, args);
+	}
+
+}
